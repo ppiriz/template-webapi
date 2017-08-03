b4ca22ebdd0efdbdfcde5c2f0db881e5a40e3ccb
Ruaidhri Mc Donnell <ruaidhri.mcdonnell@gmail.com>
2017-08-03 12:03:47 +0100

    add some AutoFac helpers

--- src/WebApi.template/Infrastructure/IoC/DefaultModule.cs
+++ src/WebApi.template/Infrastructure/IoC/DefaultModule.cs
@@ -1,4 +1,6 @@
-﻿using Autofac;
+﻿using System;
+using Autofac;
+using Autofac.Features.ResolveAnything;
 using WebApi.template.Controllers;
 
 namespace WebApi.template.Infrastructure.IoC
@@ -16,6 +18,30 @@ namespace WebApi.template.Infrastructure.IoC
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<BasicDependency>();
+
+            // http://docs.autofac.org/en/latest/register/scanning.html
+
+            // choose your convention...
+            if(false)
+            {
+                // this will scan the current assembly, and map any interfaces found on classes to resolve to that class.
+                // e.g. public class Foo : IFoo,IBar will automagically resolve BOTH IFoo and IBar to Foo
+                builder.RegisterAssemblyTypes(GetType().Assembly)
+                .AsImplementedInterfaces();
+            }
+            else
+            {
+                // similar to the above, but it will only map the interface with the same naming convention of the class.
+                // e.g. public class Foo : IFoo,IBar will automagically resolve ONLY IFoo to Foo
+                builder.RegisterAssemblyTypes(GetType().Assembly)
+                    .Where(t => WithDefaultConvention(t) != null)
+                    .As(svc => WithDefaultConvention(svc));
+            }
+
+            // this will resolve any concrete types as themselves
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
         }
+
+        private static Type WithDefaultConvention(Type t) => t.GetInterface("I" + t.Name);
     }
 }
